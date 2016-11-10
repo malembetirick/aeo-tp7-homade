0cb87e5b085657b93016b48412ad1674ca42c644
Malembeti Rick-Belfaure <rb.malembeti@etudiant.univ-lille1.fr>
2016-11-10 10:12:59 +0100

    doc nexys3v6 v1

---
+++ isim/HCU_isim_beh.exe.sim/work/HCU_isim_beh.exe_main.c
@@ -0,0 +1,52 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                       */
+/*  \   \        Copyright (c) 2003-2009 Xilinx, Inc.                */
+/*  /   /          All Right Reserved.                                 */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                      */
+/*  \___\/\___\                                                    */
+/***********************************************************************/
+
+#include "xsi.h"
+
+struct XSI_INFO xsi_info;
+
+char *IEEE_P_2592010699;
+char *STD_STANDARD;
+char *WORK_P_2431924499;
+char *IEEE_P_0774719531;
+char *IEEE_P_3620187407;
+char *IEEE_P_3499444699;
+
+
+int main(int argc, char **argv)
+{
+    xsi_init_design(argc, argv);
+    xsi_register_info(&xsi_info);
+
+    xsi_register_min_prec_unit(-12);
+    ieee_p_2592010699_init();
+    ieee_p_3499444699_init();
+    ieee_p_0774719531_init();
+    ieee_p_3620187407_init();
+    work_p_2431924499_init();
+    work_a_1734369168_3212880686_init();
+    work_a_0708679958_3212880686_init();
+
+
+    xsi_register_tops("work_a_0708679958_3212880686");
+
+    IEEE_P_2592010699 = xsi_get_engine_memory("ieee_p_2592010699");
+    xsi_register_ieee_std_logic_1164(IEEE_P_2592010699);
+    STD_STANDARD = xsi_get_engine_memory("std_standard");
+    WORK_P_2431924499 = xsi_get_engine_memory("work_p_2431924499");
+    IEEE_P_0774719531 = xsi_get_engine_memory("ieee_p_0774719531");
+    IEEE_P_3620187407 = xsi_get_engine_memory("ieee_p_3620187407");
+    IEEE_P_3499444699 = xsi_get_engine_memory("ieee_p_3499444699");
+
+    return xsi_run_simulation(argc, argv);
+
+}
